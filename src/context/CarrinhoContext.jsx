9f4aa8d2ee38e303b4061d9b766552f6f2062a4a
Neto Russo <netoacrn1@gmail.com>
2024-04-28 12:43:18 -0300

    finalizando modulo 01

---
+++ src/context/CarrinhoContext.jsx
@@ -0,0 +1,12 @@
+import React, { createContext } from "react";
+
+export const CarrinhoContext = createContext();
+
+
+export const CarrinhoProvider = ({ children }) => {
+    return (
+        <CarrinhoContext.Provider>
+            {children}
+        </CarrinhoContext.Provider>
+    )
+}
