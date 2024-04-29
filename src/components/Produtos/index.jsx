0f434e3e2e2edda085cd8674dfb560c522788ac6
Neto Russo <netoacrn1@gmail.com>
2024-04-28 22:34:24 -0300

    Subindo aula 03

--- src/components/Produtos/index.jsx
+++ src/components/Produtos/index.jsx
@@ -2,54 +2,10 @@ import React, { useContext } from "react";
 import Produto from "./Produto";
 import produtos from "@/mocks/produtos.json";
 import Titulo from "@/components/Titulo";
-import { CarrinhoContext } from "@/context/CarrinhoContext";
+import {useCarrinhoContext} from '@/hooks/useCarrinhoContext';
 
 const Produtos = () => {
-  const { carrinho, setCarrinho } = useContext(CarrinhoContext);
-
-  function adicionarProduto(novoProduto) {
-    const temOProduto = carrinho.some((itemDoCarrinho) => { 
-      itemDoCarrinho.id === novoProduto.id 
-    });
-
-    if (!temOProduto) {
-      novoProduto.quantidade = 1;
-      return setCarrinho((carrinhoAnterior) => [
-        ...carrinhoAnterior,
-        novoProduto,
-      ]);
-    }
-
-    setCarrinho((carrinhoAnterior) =>
-      carrinhoAnterior.map((itemDoCarrinho) => {
-        if (itemDoCarrinho.id === novoProduto.id) 
-          itemDoCarrinho.quantidade += 1;
-        return itemDoCarrinho;
-      })
-    );
-  }
-
-  // function removerProduto(id) {
-
-  //   const produto = carrinho.find((itemDoCarrinho) => itemDoCarrinho.id === id);
-
-  //   const ehOUltimo = produto.quantidade === 1;
-
-  //   if (ehOUltimo) {
-  //     return setCarrinho((carrinhoAnterior) => 
-  //       carrinhoAnterior.filter((itemDoCarrinho) => 
-  //         itemDoCarrinho.id !== id) 
-  //     );
-  //   }
-
-  //   setCarrinho((carrinhoAnterior) =>
-  //     carrinhoAnterior.map((itemDoCarrinho) => {
-  //       if (itemDoCarrinho.id === id) itemDoCarrinho.quantidade -= 1;
-  //       return itemDoCarrinho;
-  //     })
-  //   );
-  // }
-
+  const {adicionarProduto} = useCarrinhoContext();
   return (
     <section role="produtos" aria-label="Produtos que estão bombando!">
       <Titulo>Produtos que estão bombando!</Titulo>
---
+++ src/hooks/useCarrinhoContext.js
@@ -0,0 +1,58 @@
+import { useContext } from "react"
+import {CarrinhoContext} from '@/context/CarrinhoContext'
+
+export const useCarrinhoContext = () => {
+    const { carrinho, setCarrinho } = useContext(CarrinhoContext);
+
+    function mudarQuantidade (id, quantidade) {
+        return carrinho.map((itemDoCarrinho) => {
+            if(itemDoCarrinho.id === id) itemDoCarrinho.quantidade += quantidade;
+            return itemDoCarrinho;
+        })
+    }
+
+    function adicionarProduto(novoProduto) {
+      const temOProduto = carrinho.some((itemDoCarrinho) => { 
+        return itemDoCarrinho.id === novoProduto.id 
+      });
+  
+      if (!temOProduto) {
+        novoProduto.quantidade = 1;
+        return setCarrinho((carrinhoAnterior) => [
+          ...carrinhoAnterior,
+          novoProduto,
+        ]);
+      }
+
+      const carrinhoAtualizado = mudarQuantidade(novoProduto.id, 1);
+  
+      setCarrinho([...carrinhoAtualizado]);
+    }
+
+    function removerProduto(id) {
+        const produto = carrinho.find((itemDoCarrinho) => itemDoCarrinho.id === id);
+        const ehOUltimo = produto.quantidade === 1;
+        if (ehOUltimo) {
+          return setCarrinho((carrinhoAnterior) => 
+            carrinhoAnterior.filter((itemDoCarrinho) => 
+              itemDoCarrinho.id !== id) 
+          );
+        }
+
+        const carrinhoAtualizado = mudarQuantidade(id, -1);
+        setCarrinho([ ...carrinhoAtualizado ]);
+      }
+
+      function removerProdutoCarrinho(id) {
+        const produto = carrinho.filter((itemDoCarrinho) => (itemDoCarrinho.id !== id));
+        setCarrinho(produto);
+      }
+
+    return {
+        carrinho,
+        setCarrinho,
+        adicionarProduto,
+        removerProduto,
+        removerProdutoCarrinho
+    }
+}
